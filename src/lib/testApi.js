d03975c47d280609d020f2697aa7969862d0b336
Jeff Li <42036622+zuki08@users.noreply.github.com>
2026-03-28 18:06:04 -0400

    feat: able to get valid data from api

--- src/lib/testApi.js
+++ src/lib/testApi.js
@@ -1,8 +1,8 @@
 import { clobPriceHistoryAPI } from "./clobApi.js";
 
 async function runClobFunc() {
-	let marketId = "105104581338576429268357347529823581162598821797457643758239969314113345373365";
-	let fidelity = 10;
+	let marketId = "75922431214507703408436749060914922241512331397434519915305263983885215437544";
+	let fidelity = 2;
 	let eTs = Math.floor(Date.now() / 1000);
 	let sTs = eTs - 2 * 24 * 60 * 60 ;
 	await clobPriceHistoryAPI(marketId, sTs, eTs, fidelity);
