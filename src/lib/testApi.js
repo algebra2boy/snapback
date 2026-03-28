30e1aaab7c818b15e4cdcccca6604192754659e2
Jeff Li <42036622+zuki08@users.noreply.github.com>
2026-03-28 18:21:59 -0400

    progress: verification how to connect gamma to clob

--- src/lib/testApi.js
+++ src/lib/testApi.js
@@ -1,11 +1,19 @@
 import { clobPriceHistoryAPI } from "./clobApi.js";
+import { fetchFamilies } from "./gammaApi.js";
 
 async function runClobFunc() {
-	let marketId = "75922431214507703408436749060914922241512331397434519915305263983885215437544";
+	let marketId = tokenId; 
 	let fidelity = 2;
 	let eTs = Math.floor(Date.now() / 1000);
 	let sTs = eTs - 2 * 24 * 60 * 60 ;
 	await clobPriceHistoryAPI(marketId, sTs, eTs, fidelity);
 }
 
+async function runGammaFunc() {
+	let res = await fetchFamilies();
+	console.log(res[0].markets);
+	return JSON.parse(res[0].markets[0].clobTokenIds)[0];
+}
+
+let tokenId = await runGammaFunc();
 await runClobFunc();
